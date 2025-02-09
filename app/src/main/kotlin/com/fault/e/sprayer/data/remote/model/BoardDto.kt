9d570908ec001621064754467b762a8afd8b8cf3
FAULT-E <fault.e.droid@gmail.com>
2025-02-09 22:57:10 +0100

    Fetch boards from the network

---
+++ app/src/main/kotlin/com/fault/e/sprayer/data/remote/model/BoardDto.kt
@@ -0,0 +1,31 @@
+/**
+ * Copyright 2025 FAULT-E
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.fault.e.sprayer.data.remote.model
+
+import kotlinx.serialization.Serializable
+
+/**
+ * Board DTO
+ * @param gym Gym name
+ * @param image Board image
+ * @param name Board name
+ */
+@Serializable
+data class BoardDto(
+    val gym: String,
+    val image: String,
+    val name: String
+)
